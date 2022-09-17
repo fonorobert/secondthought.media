6ec85ea4364c5d33087a829621652b0caf045ace
Robert Fonó <fonorobert@gmail.com>
2022-09-17 14:52:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set -e
 sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
-lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftps://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
+lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
 
 #set dns:order 'inet inet6';
