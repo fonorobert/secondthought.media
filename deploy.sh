8f96235a792e76b0c500d0b991267b956877b0b2
Robert Fonó <fonorobert@gmail.com>
2022-09-17 15:37:39 +0200

    updates to travis deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set -e
 sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
-sudo lftp -d -c "set dns:order 'inet inet6'; set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public .; quit;"
+sudo lftp -d -c "set dns:order 'inet inet6'; set ftps:initial-prot ''; set ftp:ssl-force false; set ftp:ssl-protect-data true; set ssl:verify-certificate false; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -eRv public /; close -a; exit;"
 
 #set dns:order 'inet inet6';
